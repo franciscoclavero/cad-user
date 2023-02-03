06ff2d91da737df144212f250a13c56ab78d8ebb
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-02-03 14:01:11 -0300

    feat: create style and import in component

---
+++ src/components/atoms/Input/style.module.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+  outline: none;
+`;
