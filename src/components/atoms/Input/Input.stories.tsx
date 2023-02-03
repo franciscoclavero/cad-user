ef1becb68f6bfc2bedf2be0d886c1dd5eab2d334
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-02-03 14:42:24 -0300

    feat: create storybook

---
+++ src/components/atoms/Input/Input.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta } from "@storybook/react";
+
+import Input from "./Input";
+
+export default {
+  title: "Components/Input",
+  component: Input,
+} as Meta;
+
+export const Default = {};
