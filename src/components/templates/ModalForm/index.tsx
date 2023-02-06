becb2a34f2adc277f4783ead665a46a24071cd3f
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-02-06 16:24:25 -0300

    feat: create formik

--- src/components/templates/ModalForm/index.tsx
+++ src/components/templates/ModalForm/index.tsx
@@ -1,3 +1,4 @@
+import { Formik } from "formik";
 import React from "react";
 
 import Button from "../../atoms/Button";
@@ -7,20 +8,35 @@ import { BodyDiv, FlexDiv, ButtonFlex, Title } from "./style.module";
 function ModalForm() {
   return (
     <BodyDiv>
-      <Title>Name</Title>
-      <FlexDiv>
-        <InputLabel name="firstName" textLabel="First name" />
-        <InputLabel name="lastName" textLabel="Last name" />
-      </FlexDiv>
-      <Title>Address</Title>
-      <div>
-        <InputLabel name="streetLine" textLabel="Street line" />
-        <InputLabel name="streetLine2" textLabel="Street line 2" />
-        <FlexDiv>
-          <InputLabel name="city" textLabel="City" />
-          <InputLabel name="state" textLabel="State/Province" />
-        </FlexDiv>
-      </div>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          streetLine: "",
+          streetLine2: "",
+          city: "",
+          state: "",
+        }}
+        render={({ values, handleChange }) => (
+          <form>
+            <Title>Name</Title>
+            <FlexDiv>
+              <InputLabel name="firstName" textLabel="First name" />
+              <InputLabel name="lastName" textLabel="Last name" />
+            </FlexDiv>
+            <Title>Address</Title>
+            <div>
+              <InputLabel name="streetLine" textLabel="Street line" />
+              <InputLabel name="streetLine2" textLabel="Street line 2" />
+              <FlexDiv>
+                <InputLabel name="city" textLabel="City" />
+                <InputLabel name="state" textLabel="State/Province" />
+              </FlexDiv>
+            </div>
+          </form>
+        )}
+        onSubmit={() => {}}
+      />
       <ButtonFlex>
         <Button click={() => {}} />
       </ButtonFlex>
