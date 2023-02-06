894966792b4470549838a1c67c5cf1754bf04149
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-02-06 14:05:34 -0300

    feat: change InputLabe component

--- src/components/molecules/LabelInput/index.tsx
+++ src/components/molecules/LabelInput/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function LabelInput() {
-  return <div />;
+import Input from "../../atoms/Input";
+import Label from "../../atoms/Label";
+
+interface ILabelInput {
+  name: string;
+  textLabel: string;
+}
+
+function LabelInput({ name, textLabel }: ILabelInput) {
+  return (
+    <div>
+      <Label componentName={name} textContent={textLabel} />
+      <Input name={name} />
+    </div>
+  );
 }
 
 export default LabelInput;
